d1c76c25d0533079e8e64f9b6c4273155d6d5013
Juan Esteban Isaza <isazaj601@gmail.com>
2026-06-27 22:06:53 -0500

    Manipulacion de cadenas completado

--- src/main/java/com/example/ManipulacionCadenas.java
+++ src/main/java/com/example/ManipulacionCadenas.java
@@ -2,18 +2,36 @@ package com.example;
 
 public class ManipulacionCadenas {
     public static void demostrarConcatenacion() {
-        // TODO
+        String a = "Hola";
+        String b = "Mundo";
+        System.out.println(a + " " + b);
     }
 
     public static void demostrarMetodosUtiles() {
-        // TODO
+        String frase = "  Hola Mundo Java  ";
+        System.out.println(frase.trim());
+        System.out.println(frase.replace("a", "4"));
+        String[] palabras = frase.trim().split(" ");
+        for (String p : palabras) {
+            System.out.println(p);
+        }
     }
 
     public static void demostrarInmutabilidad() {
-        // TODO
+        String original = "hola";
+        String mayusculas = original.toUpperCase();
+        System.out.println(original);
+        System.out.println(mayusculas);
+        System.out.println(original == mayusculas);
     }
 
     public static void usarStringBuilder() {
-        // TODO
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hola");
+        sb.append(" ");
+        sb.append("Mundo");
+        sb.append(" ");
+        sb.append("Java");
+        System.out.println(sb.toString());
     }
 }
