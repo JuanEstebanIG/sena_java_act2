42e96f8d426add5c38b424c65c2d36bf54031769
Juan Esteban Isaza <isazaj601@gmail.com>
2026-06-27 22:03:31 -0500

    Entrada de datos completado

--- src/main/java/com/example/EntradaDeDatos.java
+++ src/main/java/com/example/EntradaDeDatos.java
@@ -4,10 +4,21 @@ import java.util.Scanner;
 
 public class EntradaDeDatos {
     public static void leerVariosTipos() {
-        // TODO
+        Scanner scanner = new Scanner(System.in);
+        String texto = scanner.nextLine();
+        double decimal = scanner.nextDouble();
+        System.out.println(texto);
+        System.out.println(decimal);
+        scanner.close();
     }
 
     public static void manejarSaltoDeLineaPendiente() {
-        // TODO
+        Scanner scanner = new Scanner(System.in);
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+        String texto = scanner.nextLine();
+        System.out.println(numero);
+        System.out.println(texto);
+        scanner.close();
     }
 }
